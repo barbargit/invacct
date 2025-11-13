78a9165b592227c291fcb170a8a347eb08b52c4a
barbargit <barjrah@gmail.com>
2025-11-13 09:10:01 +0700

    Disable standalone output in next.config.js
    Comment out output option for Vercel deployment.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone', // For Vercel deployment
+  //output: 'standalone', // For Vercel deployment
   images: {
     unoptimized: true, // For Vercel deployment
   },
